6530166a5f63e54fe0e34a90c6559f102518f76c
Artur0705 <artlil420@gmail.com>
2022-06-13 02:01:43 +1000

    Questions, answers functions finished

--- assets/script.js
+++ assets/script.js
@@ -16,6 +16,23 @@ var questions = [
         choices: ["parentheses", "quotes", "curly brackets", "square brackets"],
         answer: 0
     },
+
+    {   title: "Arrays in Javascript can be used to store ____.",
+        choices: ["numbers and strings", "other arrays", "booleans", "all of the above"],
+        answer: 3
+    },
+
+    {
+        title: "String values must be enclosed within ____ when being assigned to variables.",
+        choices: ["commas", "curly brackets", "quotes", "parenthesis"],
+        answer: 2
+    },
+
+    {
+        title: "A very useful tool for used during development and debugging for printing content to the debugger is:",
+        choices: ["Javascript", "terminal / bash", "for loops", "console log"],
+        answer: 3
+    },
 ]
 
 function hideElement (section) {
@@ -27,6 +44,13 @@ function showElement (section) {
 }
 
 function showNextQuestion() {
+    if(questions.length <= nextQuestion) {
+        finishQuiz()
+        return
+
+    }
+
+    
     var question = questions[nextQuestion]
     document.getElementById("question-title").innerHTML = question.title
     var ul = document.querySelector("#questions ul")
@@ -34,15 +58,37 @@ function showNextQuestion() {
     for(var i=0; i<question.choices.length; i++) {
         var li = document.createElement("li");
         li.innerHTML = question.choices[i];
+        li.setAttribute("data-index", i);
+        li.addEventListener("click", onOptionClick)
         ul.append(li)
 
     }
 }
 
+function onOptionClick (event) {
+    var choice = parseInt(event.target.getAttribute("data-index"))
+    var question = questions[nextQuestion]
+
+    if(choice == question.answer) {
+        answerOutcome.innerHTML = "Correct!"
+    }
+    else {
+        answerOutcome.innerHTML = "Wrong!"
+    }
+    nextQuestion++
+    showElement(answerOutcome)
+    showNextQuestion()
+}
+
 function startQuiz () {
     hideElement(startSection)
-    hideElement(answerOutcome)
     showElement(questionsSection)
     showNextQuestion()
 
 }
+
+function finishQuiz() {
+    hideElement(questionsSection)
+    showElement(allDoneSection)
+}
+
